e3e87db0c97a9f016d1d60903af8f25ff0c04596
Gema <l21590305@sjuanrio.tecnm.mx>
2025-02-11 16:01:35 -0600

    Actualizacion a alumnos

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 #ruta raiz
-@app.route('/')
+@app.route('/alumnos')
 def hola_mundo():
-    return 'Hola mundo'
+    return 'Aquí se listan los alumnos'
 if __name__ == '__main__':
     app.run(debug=True)
